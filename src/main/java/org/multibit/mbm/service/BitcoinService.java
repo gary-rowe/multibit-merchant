95c4b8c66bacd5bb66decc15a21b9bbecd58bd42
Gary Rowe <garyrowe@laptop.local>
2011-11-14 11:27:56 +0000

    Tidying comments for BitcoinService

--- src/main/java/org/multibit/mbm/service/BitcoinService.java
+++ src/main/java/org/multibit/mbm/service/BitcoinService.java
@@ -3,31 +3,32 @@ package org.multibit.mbm.service;
 import com.google.bitcoin.core.Address;
 
 /**
- * an interface encapsulating the iteraction with the Bitcoin network for MultiBitMerchant
- * @author jim
+ * <p>An interface encapsulating the iteraction with the Bitcoin network for MultiBitMerchant</p>
  *
+ * @author jim
  */
 public interface BitcoinService {
 
-    public static final long DEFAULT_TIME_TO_LIVE = 1000 * 60 * 60 * 24;    // 24 hours, in milliseconds
+  // TODO Consider using TimeUnit in java.util.concurrent
+  public static final long DEFAULT_TIME_TO_LIVE = 1000 * 60 * 60 * 24;    // 24 hours, in milliseconds
 
-    /**
-     * get the next bitcoin address from the address bucket
-     * 
-     * this is guaranteed to be a never-previously used Bitcoin address
-     * 
-     * @return nextAddress The next Bitcoin address to use.
-     *                     Null if no more addresses are available
-     */
-    public Address getNextAddress();
-    
-    /**
-     * register a Bitcoin address so that the AddressListener receives notifications
-     * of any Transactions involving this address
-     * 
-     * time to live for the registration is set to BitcoinService.DEFAULT_TIME_TO_LIVE
-     * @param address The address to register the AddressListener against
-     * @param addressListener The addressListener that will receive notifications
-     */
-    public void registerAddress(Address address, AddressListener addressListener);
+  /**
+   * <p>Get the next bitcoin address from the address bucket. This is guaranteed to be a never-previously used Bitcoin address</p>
+   *
+   * @return nextAddress The next Bitcoin address to use, or null if no more addresses are available
+   *
+   * @since 1.0.0
+   */
+  public Address getNextAddress();
+
+  /**
+   * <p>Register a Bitcoin address so that the AddressListener receives notifications
+   * of any Transactions involving this address</p>
+   *
+   * <p>Time to live for the registration is set to BitcoinService.DEFAULT_TIME_TO_LIVE</p>
+   *
+   * @param address         The address to register the AddressListener against
+   * @param addressListener The addressListener that will receive notifications
+   */
+  public void registerAddress(Address address, AddressListener addressListener);
 }
