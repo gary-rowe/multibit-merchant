6748d9e9e4e728f09c54194cc0ba02f9638284a2
jim618 <jim618@fastmail.co.uk>
2011-11-20 20:41:28 +0000

    more logging

--- src/main/java/org/multibit/mbm/service/DefaultAddressListener.java
+++ src/main/java/org/multibit/mbm/service/DefaultAddressListener.java
@@ -3,20 +3,24 @@ package org.multibit.mbm.service;
 import java.io.IOException;
 
 import org.multibit.mbm.domain.AlertMessage;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import com.google.bitcoin.core.Address;
 import com.google.bitcoin.core.Transaction;
 
 public class DefaultAddressListener implements AddressListener {
 
+  public Logger log = LoggerFactory.getLogger(DefaultAddressListener.class.getName());
+
   private Address address;
   private Long id;
-  
+
   public DefaultAddressListener(Address address, Long id) {
     this.address = address;
     this.id = id;
   }
-  
+
   @Override
   public void onCoinsReceived(Address address, Transaction transaction) {
     if (this.address.equals(address)) {
@@ -25,11 +29,11 @@ public class DefaultAddressListener implements AddressListener {
       try {
         BroadcastService.INSTANCE.broadcast(id, alertMessage);
       } catch (IOException e) {
-        // TODO Auto-generated catch block
-        e.printStackTrace();
+        log.error(e.getClass().getName() + " " + e.getMessage());
       }
     } else {
       // wrongly wired up
+      log.warn("Address listener for address " + address.toString() + " is wrongly wired up");
     }
   }
 
@@ -41,11 +45,11 @@ public class DefaultAddressListener implements AddressListener {
       try {
         BroadcastService.INSTANCE.broadcast(id, alertMessage);
       } catch (IOException e) {
-        // TODO Auto-generated catch block
-        e.printStackTrace();
+        log.error(e.getClass().getName() + " " + e.getMessage());
       }
     } else {
       // wrongly wired up
+      log.warn("Address listener for address " + address.toString() + " is wrongly wired up");
     }
   }
 
