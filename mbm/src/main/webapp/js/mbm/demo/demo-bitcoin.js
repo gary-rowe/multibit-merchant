3b5f3205ced0a1ae53aaf9032f93aace74a79b3e
Gary Rowe <g.rowe@froot.co.uk>
2012-01-15 20:49:20 +0000

    Removed all the redundant UI elements from the MBM platform module

--- mbm/src/main/webapp/js/mbm/demo/demo-bitcoin.js
+++
@@ -1,7 +0,0 @@
-// Bitcoin API bindings
-$("#bitcoin-new-address").bind("click", function(event) {
-  $.post('/mbm/api/v1/bitcoin/new-address',
-    function(data) {
-      $('#bitcoin-new-address-output').html(data);
-    });
-});
