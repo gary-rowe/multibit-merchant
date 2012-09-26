0256de668ef7fe44cff24712806ae2a254e330fd
Gary Rowe <g.rowe@froot.co.uk>
2012-09-26 21:09:30 +0100

    Fixed broken unit test and improved UserResource behaviour

--- mbm/src/test/java/org/multibit/mbm/resources/UserResourceTest.java
+++ mbm/src/test/java/org/multibit/mbm/resources/UserResourceTest.java
@@ -3,10 +3,12 @@ package org.multibit.mbm.resources;
 import com.yammer.dropwizard.testing.FixtureHelpers;
 import org.junit.Test;
 import org.multibit.mbm.api.hal.HalMediaType;
+import org.multibit.mbm.db.dto.Customer;
 import org.multibit.mbm.db.dto.User;
 import org.multibit.mbm.db.dto.UserBuilder;
 import org.multibit.mbm.services.SecurityService;
 import org.multibit.mbm.test.BaseJerseyResourceTest;
+import org.multibit.mbm.test.FixtureAsserts;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.equalTo;
@@ -17,40 +19,30 @@ import static org.mockito.Mockito.when;
 
 public class UserResourceTest extends BaseJerseyResourceTest {
 
-  private final SecurityService securityService =mock(SecurityService.class);
-  private final User expectedUser = UserBuilder
-    .newInstance()
-    .build();
-
+  private final UserResource testObject=new UserResource();
 
   @Override
   protected void setUpResources() {
 
-    // Apply mocks
-    when(securityService.getUserByUUID(anyString())).thenReturn(expectedUser);
+    // Create the User for authenticated access
+    User user = setUpAuthenticator();
+    user.setId(1L);
 
-    // Configure the test object
-    UserResource testObject = new UserResource();
-    testObject.setSecurityService(securityService);
-
+    // Configure resources
     addResource(testObject);
-
-    setUpAuthenticator();
 
   }
 
   @Test
-  public void testGetByUuid() throws Exception {
+  public void testRetrieveUser() throws Exception {
 
     String actualResponse = client()
       .resource("/user")
-      .queryParam("uuid", "abcd-1234")
       .accept(HalMediaType.APPLICATION_HAL_JSON)
       .get(String.class);
 
-    String expectedResponse= FixtureHelpers.fixture("fixtures/hal/user/expected-user-simple-jersey.json");
+    FixtureAsserts.assertStringMatchesJsonFixture("User can be retrieved as JSON", actualResponse, "fixtures/hal/user/expected-user-simple-jersey.json");
 
-    assertThat(actualResponse,is(equalTo(expectedResponse)));
   }
 
 }
