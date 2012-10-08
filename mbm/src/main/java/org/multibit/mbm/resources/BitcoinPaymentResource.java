fb51aece2f36f0aff5b31730c28456ddf1bfd20a
Gary Rowe <g.rowe@froot.co.uk>
2012-10-08 18:57:35 +0100

    MultiBitMerchant-62 Create a HMAC JS client for testing Removed conflicted Bitcoin support classes (deferring to later)

--- mbm/src/main/java/org/multibit/mbm/resources/BitcoinPaymentResource.java
+++ mbm/src/main/java/org/multibit/mbm/resources/BitcoinPaymentResource.java
@@ -11,7 +11,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 
-import javax.annotation.Resource;
 import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
