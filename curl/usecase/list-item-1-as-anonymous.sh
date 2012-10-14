b2aaf81af38590e30c5d825c1c75fc843310d3f7
Gary Rowe <g.rowe@froot.co.uk>
2012-10-14 20:46:01 +0100

    Removing the modules (again)

--- curl/usecase/list-item-1-as-anonymous.sh
+++
@@ -1,2 +0,0 @@
-# Retrieve detail on item 1
-curl -v --cookie cookies.txt --cookie-jar cookies.txt -H "Accept: application/json" "http://localhost:8080/mbm/api/v1/item/1"
--- curl/usecase/list-unbounded-items-as-anonymous.sh
+++
@@ -1,2 +0,0 @@
-# Retrieve all items
-curl -v --cookie cookies.txt --cookie-jar cookies.txt -H "Accept: application/json" "http://localhost:8080/mbm/api/v1/items"
